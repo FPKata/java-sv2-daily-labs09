f7cacb66af40af86873b29b4a1839bb20a04e1e8
FPKata <kata.percze@gmail.com>
2022-02-10 13:11:16 +0100

    StringLists

---
+++ src/main/java/day02/StringLists.java
@@ -0,0 +1,22 @@
+package day02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringLists {
+    public List<String> shortestWords(List<String> words){
+        List<String> result = new ArrayList<>();
+
+        int minLength = Integer.MAX_VALUE;
+        for (String actual : words){
+            if (actual.length() < minLength){
+                minLength = actual.length();
+                result.clear();
+            }
+            if (actual.length() == minLength){
+                result.add(actual);
+            }
+        }
+        return result;
+    }
+}
